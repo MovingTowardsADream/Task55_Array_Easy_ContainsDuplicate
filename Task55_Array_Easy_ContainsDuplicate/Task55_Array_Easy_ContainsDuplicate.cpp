8d1ec0add3f75e6e3a52e3fbd047845038fe93bb
Matvey <zxdfcvas4554@mail.ru>
2023-08-05 22:31:54 +0300

    Edit

--- Task55_Array_Easy_ContainsDuplicate/Task55_Array_Easy_ContainsDuplicate.cpp
+++ Task55_Array_Easy_ContainsDuplicate/Task55_Array_Easy_ContainsDuplicate.cpp
@@ -8,7 +8,7 @@ public:
         std::sort(nums.begin(), nums.end());
         for (int i = 1;i < nums.size();i++)
             if (nums[i - 1] == nums[i])
-                return true;
-        return false;
+                return 1;
+        return 0;
     }
 };
